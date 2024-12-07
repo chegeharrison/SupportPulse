2d19483007c3d4936b9ff16c94ca42c76a170e68
harrry <chegeharrison10@gmail.com>
2024-12-07 20:11:56 +0300

    Create base.html and all its content MOdified all the contents in the index page git commit -m

--- pulse/urls.py
+++ pulse/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   
+
     path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('features/', views.features, name='features'),
+    path('services/', views.services, name='services'),
+    path('pricing/', views.pricing, name='pricing'),
+    path('contact/', views.contact, name='contact'),
 ]
--- pulse/views.py
+++ pulse/views.py
@@ -3,3 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def features(request):
+    return render(request, 'features.html')
+
+def services(request):
+    return render(request, 'services.html')
+
+def pricing(request):
+    return render(request, 'pricing.html')
+
+def contact(request):
+    return render(request, 'contact.html')
